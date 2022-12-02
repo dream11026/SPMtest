79046beba8b23138a43091f19979093ebcca059d
張詠齊 <yungchi@cathaylife.com.tw>
2022-12-02 11:15:12 +0800

    Initial Commit

---
+++ Sources/MySPM/MySPM.swift
@@ -0,0 +1,10 @@
+public struct MySPM {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func doTest() {
+        print("is ChartManager doTest")
+    }
+}
